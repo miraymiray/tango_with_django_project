3c3d26264a4ee30aa29e31540b93c331e2e435f8
Azra Miray Atik <2890296A@student.gla.ac.uk>
2025-02-11 05:22:45 +0000

    Chapter 6 with 1 failure

--- populate_rango.py
+++ populate_rango.py
@@ -1,62 +1,55 @@
 import os
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
-
 import django
+import random
+
+# Set up Django environment
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tango_with_django_project.settings')
 django.setup()
+
 from rango.models import Category, Page
 
 def populate():
     python_pages = [
-        {'title': 'Official Python Tutorial',
-         'url': 'http://docs.python.org/3/tutorial/'},
-        {'title': 'How to Think like a Computer Scientist',
-         'url': 'http://www.greenteapress.com/thinkpython/'},
-        {'title': 'Learn Python in 10 Minutes',
-         'url': 'http://www.korokithakis.net/tutorials/python/'}
+        {'title': 'Official Python Tutorial', 'url': 'http://docs.python.org/3/tutorial/', 'views': random.randint(5, 100)},
+        {'title': 'How to Think like a Computer Scientist', 'url': 'http://www.greenteapress.com/thinkpython/', 'views': random.randint(5, 100)},
+        {'title': 'Learn Python in 10 Minutes', 'url': 'http://www.korokithakis.net/tutorials/python/', 'views': random.randint(5, 100)}
     ]
 
     django_pages = [
-        {'title': 'Official Django Tutorial',
-         'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/'},
-        {'title': 'Django Rocks',
-         'url': 'http://www.djangorocks.com/'},
-        {'title': 'How to Tango with Django',
-         'url': 'http://www.tangowithdjango.com/'}
+        {'title': 'Official Django Tutorial', 'url': 'https://docs.djangoproject.com/en/2.1/intro/tutorial01/', 'views': random.randint(5, 100)},
+        {'title': 'Django Rocks', 'url': 'http://www.djangorocks.com/', 'views': random.randint(5, 100)},
+        {'title': 'How to Tango with Django', 'url': 'http://www.tangowithdjango.com/', 'views': random.randint(5, 100)}
     ]
 
     other_pages = [
-        {'title': 'Bottle',
-         'url': 'http://bottlepy.org/docs/dev/'},
-        {'title': 'Flask',
-         'url': 'http://flask.pocoo.org'}
+        {'title': 'Bottle', 'url': 'http://bottlepy.org/docs/dev/', 'views': random.randint(5, 100)},
+        {'title': 'Flask', 'url': 'http://flask.pocoo.org', 'views': random.randint(5, 100)}
     ]
 
     cats = {
-        'Python': {'pages': python_pages, 'views': 128, 'likes': 64},
-        'Django': {'pages': django_pages, 'views': 64, 'likes': 32},
-        'Other Frameworks': {'pages': other_pages, 'views': 32, 'likes': 16}
+        'Python': {'pages': python_pages, 'views': 15, 'likes': 56},
+        'Django': {'pages': django_pages, 'views': 60, 'likes': 90},
+        'Other Frameworks': {'pages': other_pages, 'views': 2, 'likes': 17}
     }
 
     for cat, cat_data in cats.items():
-        c = add_cat(cat, cat_data['views'], cat_data['likes'])
+        c = add_category(cat, cat_data['views'], cat_data['likes'])
         for p in cat_data['pages']:
-            add_page(c, p['title'], p['url'])
+            add_page(c, p['title'], p['url'], p['views'])
 
-    for c in Category.objects.all():
-        for p in Page.objects.filter(category=c):
-            print(f'- {c}: {p}')
+    print("Database population complete!")
 
 def add_page(cat, title, url, views=0):
-    p = Page.objects.get_or_create(category=cat, title=title)[0]
+    p, created = Page.objects.get_or_create(category=cat, title=title)
     p.url = url
-    p.views = views
+    p.views = views  # Assigning views
     p.save()
     return p
 
-def add_cat(name, views=0, likes=0):
+def add_category(name, views=0, likes=0):
     c, created = Category.objects.get_or_create(name=name, defaults={'views': views, 'likes': likes})
     return c
 
 if __name__ == '__main__':
-    print('Starting Rango population script...')
+    print("Starting Rango population script...")
     populate()
--- rango/views.py
+++ rango/views.py
@@ -1,12 +1,41 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-
-
-
+from django.shortcuts import render
+from rango.models import Category, Page
 
 def index(request):
-    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    category_list = Category.objects.order_by('-likes')[:5]  # Top 5 most liked categories
+    page_list = Page.objects.order_by('-views')[:5]  # Top 5 most viewed pages
+
+    context_dict = {
+        'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!',
+        'categories': category_list,
+        'pages': page_list
+    }
+
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
     return render(request, 'rango/about.html')
+
+def show_category(request, category_name_slug):
+    # Create a context dictionary to pass to the template
+    context_dict = {}
+
+    try:
+        # Try to get the category with the given slug
+        category = Category.objects.get(slug=category_name_slug)
+
+        # Retrieve associated pages
+        pages = Page.objects.filter(category=category)
+
+        # Add retrieved data to the context dictionary
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+
+    except Category.DoesNotExist:
+        # If category doesn't exist, set values to None
+        context_dict['category'] = None
+        context_dict['pages'] = None
+
+    # Render and return response
+    return render(request, 'rango/category.html', context=context_dict)
