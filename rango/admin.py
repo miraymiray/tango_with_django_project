f32d1e36b0e1cb9ef62ae722737e503d95f880e9
Azra Miray Atik <2890296A@student.gla.ac.uk>
2025-02-14 13:25:22 +0000

    10 Chapter successful

--- rango/admin.py
+++ rango/admin.py
@@ -7,4 +7,5 @@ class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug':('name',)}
 
 admin.site.register(Category, CategoryAdmin)
+admin.site.register(Page)
 admin.site.register(UserProfile)
